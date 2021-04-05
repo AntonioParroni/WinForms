5d2d2bbb0850a75054a527d9e1f1576dbeea1c46
Antonio <parroniantonio@gmail.com>
2021-04-05 15:01:52 +0300

    EntityFramework

---
+++ CRUD/CRUD/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(81, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(617, 281);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(35, 30);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(108, 36);
+            this.ConnectButton.TabIndex = 1;
+            this.ConnectButton.Text = "Connect to DB";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listBox1.Location = new System.Drawing.Point(179, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 56);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Table";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(434, 42);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(88, 24);
+            this.CreateButton.TabIndex = 4;
+            this.CreateButton.Text = "Create";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(555, 42);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(88, 24);
+            this.UpdateButton.TabIndex = 5;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(680, 42);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(88, 24);
+            this.DeleteButton.TabIndex = 6;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.CreateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button DeleteButton;
+    }
+}
+
---
+++ CRUD/CRUD/Form1.cs
@@ -0,0 +1,169 @@
+﻿/*Довольно долго мучался с этой домашкой и созданием более динамического и лаконичного подхода к ней.
+Но кажется, что в данном случае, прийдется прописывать для каждой отдельной таблицы (которых у меня больше чем трех) 
+собственную логику обработки всего, плюс индивидуальное добавление и редактирование для каждой. 
+Мне просто лень, извините. Кажется принцип я понял. Но если конечно есть способ сделать все это намного проще.. 
+я бы хотел это узнать.
+
+Используем на db.МояТаблица.Find(id);
+И вызываем соответсвующие CRUD методы db.МояТаблица.Remove(x);
+db.SaveChanges();*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+
+        public AcademyEntities db; 
+        public Form1()
+        {
+            InitializeComponent();
+            
+            label1.Hide();
+            listBox1.Hide();
+            CreateButton.Hide();
+            UpdateButton.Hide();
+            DeleteButton.Hide();
+
+            db = new AcademyEntities();
+            
+            /*db.Curators.Load();
+
+            dataGridView1.DataSource = db.Curators.Local.ToBindingList();*/
+
+
+            /*  var tableNames = db.MetadataWorkspace.GetItems(DataSpace.SSpace)
+                          .Select(t => t.Name)
+                          .ToList();
+            DbSet dbSet = db.;*/
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           db = new AcademyEntities();
+
+            listBox1.Show();
+            
+            var metadata = ((IObjectContextAdapter)(DbContext)db).ObjectContext.MetadataWorkspace;
+            var tables = metadata.GetItemCollection(DataSpace.SSpace)
+                .GetItems<EntityContainer>()
+                .Single()
+                .BaseEntitySets
+                .OfType<EntitySet>()
+                .Where(s => !s.MetadataProperties.Contains("Type")
+                || s.MetadataProperties["Type"].ToString() == "Tables");
+
+            listBox1.DataSource = tables.ToList();
+            
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CreateButton.Show();
+            DeleteButton.Show();
+            UpdateButton.Show();
+            string categoryName = listBox1.SelectedItem.ToString();
+
+            //Type returnType;
+            switch (categoryName)
+            {
+                case "Curators":
+                    db.Curators.Load();
+                    dataGridView1.DataSource = db.Curators.Local.ToBindingList();
+                    break;
+                case "Departments":
+                    db.Departments.Load();
+                    dataGridView1.DataSource = db.Departments.Local.ToBindingList();
+                    break;
+                case "Faculties":
+                    db.Faculties.Load();
+                    dataGridView1.DataSource = db.Faculties.Local.ToBindingList();
+                    break;
+                case "Groups":
+                    db.Groups.Load();
+                    dataGridView1.DataSource = db.Groups.Local.ToBindingList();
+                    break;
+                case "GroupsCurators":
+                    db.GroupsCurators.Load();
+                    dataGridView1.DataSource = db.GroupsCurators.Local.ToBindingList();
+                    break;
+                case "GroupsLectures":
+                    db.GroupsLectures.Load();
+                    dataGridView1.DataSource = db.GroupsLectures.Local.ToBindingList();
+                    break;
+                case "Lectures":
+                    db.Lectures.Load();
+                    dataGridView1.DataSource = db.Lectures.Local.ToBindingList();
+                    break;
+                case "Subjects":
+                    db.Subjects.Load();
+                    dataGridView1.DataSource = db.Subjects.Local.ToBindingList();
+                    break;
+                case "Teachers":
+                    db.Teachers.Load();
+                    dataGridView1.DataSource = db.Teachers.Local.ToBindingList();
+                    break; 
+            }
+            /*
+            db.Set(returnType);
+            dataGridView1.DataSource = db.GetTableType(categoryName).Local;
+            dataGridView1.DataSource = db.Curators.Local.ToBindingList();
+
+            var dt = new DataTable();
+            var conn = db.Database.Connection;
+            var connectionState = conn.State;
+                if (connectionState != ConnectionState.Open) 
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = $"SELECT * FROM {categoryName}";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dt.Load(reader);
+                    }
+                }
+            dataGridView1.DataSource = dt;
+
+
+            button2.Show();
+            var dynamicTableType = Type.GetType(categoryName);
+            dataGridView1.DataSource = db.Set(dynamicTableType.GetType);
+            DbSet asd = new DbSet();
+            db.Curators.Local.tob*/
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            db.SaveChanges();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
